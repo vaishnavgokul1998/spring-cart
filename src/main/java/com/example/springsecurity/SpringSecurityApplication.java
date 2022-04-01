29be34cfd7e2b86eaaa59fbe62270413d9809540
vaishnavgokul1998 <71577139+vaishnavgokul1998@users.noreply.github.com>
2022-04-01 16:38:25 +0530

    set time zone

--- src/main/java/com/example/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/example/springsecurity/SpringSecurityApplication.java
@@ -1,14 +1,25 @@
 package com.example.springsecurity;
 
+import java.util.Date;
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		// Setting Spring Boot SetTimeZone
+		TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+		System.out.println("Spring boot application running in UTC timezone :" + new Date());
+	}
 }
